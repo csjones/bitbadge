024aba1e8e2e05922ff27224f50c6d27ddecd59c
Chris <crookiemoonster+github@gmail.com>
2014-05-26 15:22:21 -0400

    Setup

---
+++ BitBadge for iOS/BitBadge/Controllers/MenuVC.h
@@ -0,0 +1,13 @@
+//
+//  MenuVC.h
+//  BitBadge
+//
+//  Created by Chris on 5/24/14.
+//  Copyright (c) 2014 GigaBitcoin, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuVC : UIViewController
+
+@end
---
+++ BitBadge for iOS/BitBadge/Controllers/ScanVC.m
@@ -0,0 +1,40 @@
+//
+//  ScanVC.m
+//  BitBadge
+//
+//  Created by Chris on 5/24/14.
+//  Copyright (c) 2014 GigaBitcoin, LLC. All rights reserved.
+//
+
+#import "ScanVC.h"
+
+@interface ScanVC ()
+
+@end
+
+@implementation ScanVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
