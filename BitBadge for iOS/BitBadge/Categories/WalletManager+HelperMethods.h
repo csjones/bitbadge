3f436252cc6dcb2068fabad0062b4cca94272010
Chris <crookiemoonster+github@gmail.com>
2014-06-17 01:12:36 -0400

    Added helper methods

---
+++ BitBadge for iOS/BitBadge/Categories/WalletManager+HelperMethods.h
@@ -0,0 +1,15 @@
+//
+//  WalletManager+HelperMethods.h
+//  BitBadge
+//
+//  Created by Chris on 6/15/14.
+//  Copyright (c) 2014 GigaBitcoin, LLC. All rights reserved.
+//
+
+#import "WalletManager.h"
+
+@interface WalletManager ( HelperMethods )
+
+- ( NSUInteger )numberOfMatchesForString:( NSString* )string;
+
+@end
---
+++ BitBadge for iOS/BitBadge/Categories/WalletManager+HelperMethods.m
@@ -0,0 +1,31 @@
+//
+//  WalletManager+HelperMethods.m
+//  BitBadge
+//
+//  Created by Chris on 6/15/14.
+//  Copyright (c) 2014 GigaBitcoin, LLC. All rights reserved.
+//
+
+#import "WalletManager+HelperMethods.h"
+
+@implementation WalletManager ( HelperMethods )
+
+- ( NSUInteger )numberOfMatchesForString:( NSString* )string
+{
+    NSUInteger count = 0;
+    
+    __weak NSArray* weakWallets = self.keychains[ self.activeKeychain.integerValue ][ @"info" ][ @"wallets" ];
+    
+    for ( __weak NSDictionary* weakWallet in weakWallets )
+        for ( __weak NSString* weakAddress in weakWallet.allValues[ 0 ])
+        {
+            NSRange result = [weakAddress rangeOfString:string];
+            
+            if ( result.location != NSNotFound )
+                count++;
+        }
+    
+    return count;
+}
+
+@end
